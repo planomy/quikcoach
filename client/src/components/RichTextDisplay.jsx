da1ad539f060282f79c02950f5feb32fd277998b
planomy <nic@eq.edu.au>
2026-08-25 12:57:19 +1000

    Preserve whitespace in broadcast text

--- client/src/components/RichTextDisplay.jsx
+++ client/src/components/RichTextDisplay.jsx
@@ -4,7 +4,7 @@ import { richHtmlToPlainText, sanitizeRichHtml } from '../lib/richText.js';
 function PlainTextBlocks({ text = '', className = '' }) {
   const lines = String(text || '').replace(/\r\n?/g, '\n').split('\n');
   return (
-    <div className={`break-words ${className}`}>
+    <div className={`whitespace-pre-wrap break-words ${className}`}>
       {lines.map((line, index) => (
         <div key={index} className={index > 0 ? 'mt-2 min-h-[1em]' : 'min-h-[1em]'}>
           {line || '\u00a0'}
@@ -26,7 +26,7 @@ export default function RichTextDisplay({ html, text = '', className = '' }) {
 
   return (
     <div
-      className={`break-words leading-relaxed [&_div]:min-h-[1em] [&_div+div]:mt-2 [&_p]:min-h-[1em] [&_p+p]:mt-2 [&_li+li]:mt-1 [&_ul]:list-disc [&_ul]:pl-5 [&_ol]:list-decimal [&_ol]:pl-5 [&_mark]:rounded-sm [&_mark]:bg-yellow-200 [&_mark]:px-0.5 [&_mark]:text-inherit ${className}`}
+      className={`whitespace-pre-wrap break-words leading-relaxed [&_div]:min-h-[1em] [&_div+div]:mt-2 [&_p]:min-h-[1em] [&_p+p]:mt-2 [&_li+li]:mt-1 [&_ul]:list-disc [&_ul]:pl-5 [&_ol]:list-decimal [&_ol]:pl-5 [&_mark]:rounded-sm [&_mark]:bg-yellow-200 [&_mark]:px-0.5 [&_mark]:text-inherit ${className}`}
       dangerouslySetInnerHTML={{ __html: safeHtml }}
     />
   );
