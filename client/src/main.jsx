539b17cc86bc42892e209f7f768593d338271a72
planomy <nic@eq.edu.au>
2026-09-04 08:55:35 +1000

    Load Sets library layout fixes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { ThemeProvider } from './lib/theme.jsx';
 import './index.css';
+import './styles/setsLibrary.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
